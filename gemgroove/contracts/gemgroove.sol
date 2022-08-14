dc76fd945b1035fd396462e7a7e2d06aa1363ad1
Jesse21 <98306896+Jesse-21@users.noreply.github.com>
2022-08-14 07:12:22 -0400

    Update gemgroove.sol
    changed name

--- gemgroove/contracts/gemgroove.sol
+++ gemgroove/contracts/gemgroove.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
 
 contract GemGrooveCoin is ERC721Full {
-    constructor() public ERC721Full("GemGrooveCoin", "GGC") { }
+    constructor() public ERC721Full("Music2Blockchain", "M2B") { }
 
     function registerGemgroove(address owner, string memory tokenURI)
         public
