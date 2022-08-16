83acd396431b27c364e98d0ff3bfa961edd2a918
Jesse21 <98306896+Jesse-21@users.noreply.github.com>
2022-08-16 09:50:47 -0400

    Update music2blockchain.sol

--- gemgroove/contracts/music2blockchain.sol
+++ gemgroove/contracts/music2blockchain.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
 
-contract M2BC-song is ERC721Full {
+contract M2BCsong is ERC721Full {
     constructor() public ERC721Full("M2BC-song", "M2B") { }
 
     function registerMusic2Blockchain(address owner, string memory tokenURI)
